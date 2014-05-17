8e714a0630e81abbe741054981f4b6c0391e94ea
DJGriego <daniel@jackrabbitsystems.com>
2014-05-17 17:50:45 -0400

    added new styles, font, and updated home html design

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <script type="text/javascript" src="vendor/angular-1.2.16/angular.js"></script>
 <script type="text/javascript" src="vendor/angular-1.2.16/angular-route.js"></script>
 <script type="text/javascript" src="js/controllers.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Raleway:700,400,300,200' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="styles/bluewild.css">
+<link rel="stylesheet" href="styles/normalize.css">
 <link rel="SHORTCUT ICON" href="favicon.ico" />
 <title>Blue Wild - Scuba Diving</title>
 </head>
 <body ng-app="bw2">
-<div class="top-bar">
-    <div class="top-bar-wrapper">
-        <div class="title-left">
-            <h1>Under Construction</h1>
-        </div>
+<header>
+    <div class="header-wrapper">
+    	<h1 id="logo">blue wild us</h1>
     </div>
-</div>
+</header>
 
 <div ng-view></div>
 
