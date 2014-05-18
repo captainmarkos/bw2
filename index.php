678ae27f3e1e422a1a347cff127ca0cd67f5aca8
Mark Brettin <mark@jackrabbitsystems.com>
2014-05-18 15:58:07 -0400

    HTML validation fixes.

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script type="text/javascript" src="vendor/angular-1.2.16/angular.js"></script>
-<script type="text/javascript" src="vendor/angular-1.2.16/angular-route.js"></script>
+<script type="text/javascript" src="vendor/angular-1.2.16/angular.min.js"></script>
+<script type="text/javascript" src="vendor/angular-1.2.16/angular-route.min.js"></script>
 <script type="text/javascript" src="js/controllers.js"></script>
-<link rel="stylesheet" href="vendor/font-awesome-4.1.0/css/font-awesome.min.css">
-<link href='http://fonts.googleapis.com/css?family=Raleway:700,500,400,300,200' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="styles/bluewild.css">
-<link rel="stylesheet" href="styles/bluewild-devices.css">
-<link rel="stylesheet" href="styles/normalize.css">
+<link rel="stylesheet" href="vendor/font-awesome-4.1.0/css/font-awesome.min.css" />
+<link href="http://fonts.googleapis.com/css?family=Raleway:700,500,400,300,200" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="styles/bluewild.css" />
+<link rel="stylesheet" href="styles/bluewild-devices.css" />
+<link rel="stylesheet" href="styles/normalize.css" />
 <link rel="SHORTCUT ICON" href="favicon.ico" />
 <title>Blue Wild - Scuba Diving</title>
 </head>
