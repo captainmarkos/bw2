b07e2f4715ec15e22fec68e64a3512fb84c80df0
Mark Brettin <mark@jackrabbitsystems.com>
2016-06-17 13:06:15 -0600

    Add the dive schedule admin

--- admin/dive_schedule.php
+++ admin/dive_schedule.php
@@ -6,19 +6,38 @@ require_once('db_helper.php');
 $db_helper = new DBHelper($dbconn);
 
 // Get all the dive sites so we can construct a select list.
-$sql  = "SELECT * FROM dive_sites";
-//$params = array($divedata['email'], $divedata['dive_no']);
-//$sql = $db_helper->construct_secure_query($sql, $params);
+$sql = "SELECT * FROM dive_sites ORDER BY name";
+$sql = $db_helper->construct_secure_query($sql, array());
 $res = $dbconn->query($sql);
 if(!$res) {
     echo "ERROR: Query Failed: errno: " . $dbconn->errno . " error: " . $dbconn->error;
     exit();
 }
 
-$dive_sites_select_list = array();
+$dive_sites = array();
 while($row = $res->fetch_assoc()) {
-    $a = array($row['id'] => $row['name']);
-    array_push($dive_sites_select_list, $a);
+    $a = array('id' => $row['id'], 'name' => $row['name']);
+    array_push($dive_sites, $a);
+}
+
+//echo 'dive_site: ' . $_REQUEST['dive_site']; echo '<br/>';
+//echo 'charter_time: ' . $_REQUEST['charter_time']; echo '<br/>';
+//echo 'charter_date: ' . $_REQUEST['charter_date']; echo '<br/>';
+//echo 'charter_save: ' . $_REQUEST['charter_save']; echo '<br/>';
+
+if(isset($_REQUEST['charter_save']) && $_REQUEST['charter_save'] == 'Save') {
+    // Save to the database
+    $sql  = "INSERT INTO charter_schedules (dive_site1, dive_site2, charter_time, ";
+    $sql .= "charter_date) VALUES (?, ?, ?, ?)";
+    $params = array($_REQUEST['dive_site1'],
+                    $_REQUEST['dive_site2'],
+                    $_REQUEST['charter_time'],
+                    $_REQUEST['charter_date']);
+    $sql = $db_helper->construct_secure_query($sql, $params);
+    $res = $dbconn->query($sql);
+    if(!$res) {
+        echo "ERROR: Query Failed: errno: " . $dbconn->errno . " error: " . $dbconn->error;
+    }
 }
 
 ?>
@@ -32,13 +51,75 @@ while($row = $res->fetch_assoc()) {
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Raleway:400" />
 <link type="text/css" rel="stylesheet" href="/vendor/font-awesome-4.1.0/css/font-awesome.min.css" />
+<link type="text/css" rel="stylesheet" href="/vendor/jquery-ui-1.11.4.custom/jquery-ui.theme.css" />
+<link type="text/css" rel="stylesheet" href="/vendor/jquery-ui-1.11.4.custom/jquery-ui.css" />
 <link type="text/css" rel="stylesheet" href="/styles/foundation.css" />
 <link type="text/css" rel="stylesheet" href="/styles/normalize.css" />
 <link type="text/css" rel="stylesheet" href="/styles/bluewild.css" />
 <link type="image/x-icon" rel="SHORTCUT ICON" href="/favicon.ico" />
 <title>Blue Wild Scuba: dive schedule</title>
 <script type="text/javascript" src="/vendor/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="/vendor/jquery-ui-1.11.4.custom/jquery-ui.min.js"></script>
 <script type="text/javascript" src="/js/bluewildscuba.js"></script>
+<script type="text/javascript">
+
+    function validate() {
+        console.log('--> charter-date: ' + $('#charter-date').val());
+        console.log('--> dive-site1: ' + $('#dive-site1').val());
+        console.log('--> dive-site2: ' + $('#dive-site2').val());
+        console.log('--> charter-time: ' + $('#charter-time').val());
+
+        if(!$('#charter-date').val() || !$('#dive-site1').val() ||
+           !$('#dive-site2').val() || !$('#charter-time').val()) {
+            alert('All fields need to be filled out.');
+            $('#charter-form').submit(function(e) {
+                e.preventDefault();
+            });
+        }
+    }
+
+
+$(function() {
+
+    var d = new Date();
+    var month = d.getMonth() +1;
+        month = (month < 10) ? '0' + month : month;
+    var today = d.getFullYear() + '-' + month + '-' + d.getDate();
+    //$('#charter-date').val(today);
+
+    $('#charter-date').datepicker({
+        dateFormat: 'yy-mm-dd',
+        minDate: 0
+    });
+});
+
+</script>
+<style>
+
+#dive-site1, #dive-site2 {
+    color: #000000;
+    width: 300px;
+}
+
+#charter-date {
+    width: 100px;
+    display: inline-block;
+    color: #000000;
+}
+
+#charter-time {
+    width: 100px;
+    display: inline-block;
+    color: #000000;
+}
+
+#charter-save {
+    color: #000000;
+    margin-left: 25px;
+}
+
+</style>
+
 </head>
 <body>
     <header>
@@ -66,16 +147,16 @@ while($row = $res->fetch_assoc()) {
         <div class="large-12 column nav">
             <ul class="inline-list">
               <li class="no-margin-left"><a href="/"><i class="fa fa-home icon-font-size"></i></a></li>
-              <li><a href="/courses.html">Scuba Courses</a></li>
               <li><a href="/aboutus.html">About Us</a></li>
-              <li><a href="/divelog/index.php" target="_self" class="hide-for-small-only">Dive Log</a></li>
+              <li><a href="/charters.html">Charters</a></li>
+              <li><a href="/dive_schedule.html">Dive Schedule</a></li>
+              <!-- <li><a href="/divelog/index.php" target="_self" class="hide-for-small-only">Dive Log</a></li> -->
               <li><a href="/reefcreatures/index.php" target="_self">Reef Creature Quiz</a></li>
               <!-- <li style="float: right;"><span id="facebook-like-button2"></span></li> -->
             </ul>
         </div>
     </div>
 
-<!-- BEGIN: home.html -->
 </br>
 <div class="row">
   <div class="">
@@ -88,18 +169,41 @@ while($row = $res->fetch_assoc()) {
 <div class="row">
     <div class="small-12 large-12 columns no-padding antialised">
         <div class="panel">
-            <p>
-              <script type="text/javascript">
-                  var dive_date = moment('2016-05-30').format('dddd MMMM DD YYYY');
-                  var dive_time = '8:00 AM';
-                  document.write(dive_date + ' | ' + dive_time);
-              </script>
-              </br>
-              </br>
-              <a href="">Hog Heaven</a> | depth 70ft
-              </br>
-              <a href="">Reef</a> | depth 30ft
-            </p>
+
+            <form action="dive_schedule.php" method="post" id="charter-form">
+                Date: <input type="text" name="charter_date" id="charter-date">
+
+                Time: <select id="charter-time" name="charter_time">
+                          <option value=""></option>
+                          <option value="AM">AM</option>
+                          <option value="PM">PM</option>
+                      </select>
+                <br />
+                Dive Site 1: <select id="dive-site1" name="dive_site1">
+                                 <option value=""></option>
+                             <?php
+                                 foreach($dive_sites as $site) {
+                                     echo '<option value="' . $site['name'] . '">' .
+                                     $site['name'] . '</option>';
+                                 }
+                             ?>
+                             </select>
+                <br />
+                <br />
+                Dive Site 2: <select id="dive-site2" name="dive_site2">
+                                 <option value=""></option>
+                             <?php
+                                 foreach($dive_sites as $site) {
+                                     echo '<option value="' . $site['name'] . '">' .
+                                     $site['name'] . '</option>';
+                                 }
+                             ?>
+                             </select>
+                <br />
+                <br />
+                <input type="submit" onclick="validate();" id="charter-save" name="charter_save" value="Save" />
+            </form>
+
         </div>
     </div>
 </div>
